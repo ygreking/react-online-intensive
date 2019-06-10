47b21d4fbf5f8abfafe4ddc4819174813d6bf3c7
Igor Kharabet <user@Igors-MacBook-Pro.local>
2019-06-10 18:39:44 +0300

    remove unused code from StatusBar

--- source/components/StatusBar/index.js
+++ source/components/StatusBar/index.js
@@ -9,7 +9,6 @@ import Styles from './styles.m.css';
 
 export default class StatusBar extends Component {
     render() {
-        const { currentUserFirstName, currentUserLastName, avatar } = this.props;
 
         return (
             <Consumer>
