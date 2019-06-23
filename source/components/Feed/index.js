232d0b77d5433499e7580cdb51a161ef6fd35560
Igor Kharabet <user@Igors-MacBook-Pro.local>
2019-06-23 18:39:15 +0300

    added posts show/hide animations

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,6 +1,6 @@
 // Core
 import React, { Component } from 'react';
-import { Transition } from 'react-transition-group';
+import { Transition, CSSTransition, TransitionGroup } from 'react-transition-group';
 import { fromTo } from 'gsap';
 
 // Components
@@ -169,14 +169,24 @@ export default class Feed extends Component {
 
         const postsJSX = posts.map((post) => {
             return (
-                <Catcher key = { post.id }>
-                    <Post
-                        key = { post.id }
-                        { ...post }
-                        _likePost = { this._likePost }
-                        _removePost = { this._removePost }
-                    />
-                </Catcher>
+                <CSSTransition
+                    classNames = {{
+                        enter:       Styles.postInStart,
+                        enterActive: Styles.postInEnd,
+                        exit:        Styles.postOutStart,
+                        exitActive:  Styles.postOutEnd,
+                    }}
+                    key = { post.id }
+                    timeout = {{ enter: 500, exit: 400 }}>
+                    <Catcher>
+                        <Post
+                            key = { post.id }
+                            { ...post }
+                            _likePost = { this._likePost }
+                            _removePost = { this._removePost }
+                        />
+                    </Catcher>
+                </CSSTransition>
             );
         });
 
@@ -195,7 +205,7 @@ export default class Feed extends Component {
                     _hidePostman = { this._hidePostman }
                     in = { showPostman }
                 />
-                {postsJSX}
+                <TransitionGroup>{postsJSX}</TransitionGroup>
             </section>
         );
     }
